1a4a7833448158976591a899ea855882d3ebeb98
Subhashish Barik <subhashishb130@gmail.com>
2026-08-04 19:28:31 +0530

    Baseline Model 1: Logistic Regression

--- customer_churn_prediction.py
+++ customer_churn_prediction.py
@@ -125,3 +125,15 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 print("Train size:", X_train.shape, " Test size:", X_test.shape)
+
+
+
+scaler = StandardScaler()                                                             //Logistic Regression
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+log_reg = LogisticRegression(max_iter=2000, random_state=42)
+log_reg.fit(X_train_scaled, y_train)
+y_pred_lr = log_reg.predict(X_test_scaled)
+y_proba_lr = log_reg.predict_proba(X_test_scaled)[:, 1]
+print("Logistic Regression Accuracy:", accuracy_score(y_test, y_pred_lr))
+print(classification_report(y_test, y_pred_lr, target_names=["No Churn", "Churn"]))
